389e318e10f825d1dcd55a13fb4ced57883845ee
PC <corno@alum.mit.edu>
2021-06-30 13:56:46 +0100

    Add lookup dictionary creation

--- spond/experimental/glove/inspect_results.py
+++ spond/experimental/glove/inspect_results.py
@@ -1,21 +1,75 @@
 import os
 import pandas as pd
+import sys
 
-from spond.experimental.glove.probabilistic_glove import ProbabilisticGlove
 
 import socket
 if socket.gethostname().endswith('pals.ucl.ac.uk'):
-    # set up pythonpath
-    ppath = '/home/petra/spond'
     # set up data path
-    datapath = '/home/petra/data'
+    datapath = "/home/petra/spond/spond/experimental/glove/results"
+
 else:
     1/0
 
-datapath = "/home/petra/spond/spond/experimental/glove/results"
+from spond.experimental.openimages.readfile import readlabels
+
+datafiles = {
+    'openimages': {
+        'all_labels':  os.path.join(datapath, 'openimages', 'oidv6-class-descriptions.csv'),
+        'included_labels': pd.read_csv(
+            os.path.join(datapath, 'openimages', 'class_labels_indices.csv'),
+            index_col=0
+        )
+    },
+    'audioset': {
+        'all_labels':  os.path.join(datapath, 'audioset', 'all_labels.csv'),
+        'included_labels': pd.DataFrame({
+            'mid': pd.Series(index_to_label),
+            'display_name': pd.Series(index_to_name)
+        })
+    },
+}
+
+lookup = {
+    'openimages': {}, 'audioset': {},
+}    
+
 
 tags = ("openimages", "audioset")
 
+
+for tag in tags:
+    labelsfn = datafiles[tag]['all_labels']
+    # labels = machine IDs to index
+    # names = category name to index
+    labels, names = readlabels(labelsfn, rootdir=None)
+    name_to_label = {v: k for k, v in names.items()}
+    index_to_label = {v: k for k, v in labels.items()}
+    index_to_name = {v: names[k] for k, v in labels.items()}
+    name_to_index = {v: k for k, v in index_to_name.items()}
+    keep = np.array([labels[label] for label in datafiles['included_labels']['mid'].values])
+    # index of label in file to index in the output embedding
+    lookup[tag]['label_to_index'] = labels
+    lookup[tag]['name_to_index'] = name_to_index
+    # the audioset embeddings are for all labels,
+    # so use the 'keep' array to further filter only the ones we want. 
+    # for openimages it has no effect
+    lookup[tag]['included_index'] = keep
+    lookup[tag]['included_names'] = [lookup[tag][index_to_name][ind] for ind in keep]
+
+# now find the labels that are in both domains, and the indexes of those labels
+# in the embeddings. 
+union = [
+    item for item in lookup['audioset']['name_to_label'].keys()
+    if item in lookup['openimages']]['included_names']
+]
+
+for tag in tags:
+    lookup[tag]['union'] = {
+        name: lookup[tag]['name_to_index'][name] for name in union
+    }
+
+    
 stores = {
     tag: pd.HDFStore(os.path.join(datapath, tag, "ProbabilisticGlove", f"{tag}_analytics.hdf5"), 'r')
     for tag in tags 
